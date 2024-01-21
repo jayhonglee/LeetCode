c5c71d431ad624b594c303392c97ec53e3150af9
Jaehong Lee <contact@jaehonglee.com>
2024-01-20 20:22:54 -0800

    Time: 65 ms (52.35%), Space: 56.5 MB (5.07%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    function applySameStyle(str) {
+        let hashmap = new Map(), finalStr = "", i = 0;
+        
+        for(const char of str) {
+            if(!hashmap.has(char)) {
+                hashmap.set(char, i);        
+                i++;
+            }
+            
+            finalStr += (` ${hashmap.get(char)}`);
+        }
+        
+        console.log(finalStr);
+        return finalStr;
+    }
+    
+    return applySameStyle(s) === applySameStyle(t);
+};
