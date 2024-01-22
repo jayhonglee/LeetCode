c2b6a494c16188eb9e4ab5ede4f97859ddd0f1aa
Jaehong Lee <contact@jaehonglee.com>
2024-01-21 18:52:16 -0800

    Time: 135 ms (59.71%), Space: 55.4 MB (9.20%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.js
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.js
@@ -13,11 +13,15 @@ var closeStrings = function(word1, word2) {
         return hashmap;
     }
     
+    function sortAndJoin (array) {
+        return array.sort().join(" ");
+    }
+    
     let ans = true;
     
     const word1HashmapArray = [...createHashmap(word1)];
     const word2HashmapArray = [...createHashmap(word2)];
         
-    return word1HashmapArray.map(x=> x[1]).sort().join(" ") === word2HashmapArray.map(x=>x[1]).sort().join(" ") && word1HashmapArray.map(x=> x[0]).sort().join(" ") === word2HashmapArray.map(x=>x[0]).sort().join(" ");
+    return sortAndJoin(word1HashmapArray.map(x=> x[1])) === word2HashmapArray.map(x=>x[1]).sort().join(" ") && word1HashmapArray.map(x=> x[0]).sort().join(" ") === word2HashmapArray.map(x=>x[0]).sort().join(" ");
         
 };
