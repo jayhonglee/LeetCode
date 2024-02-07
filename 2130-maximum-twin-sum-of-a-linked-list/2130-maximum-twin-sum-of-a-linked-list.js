00d636e2c2f6ae725213cde9b31f4af5ecb14199
Jaehong Lee <contact@jaehonglee.com>
2024-02-07 14:12:19 -0800

    Time: 1155 ms (5.02%), Space: 79.1 MB (69.49%) - LeetHub

---
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var pairSum = function(head) {
+//     Find the half point
+    let slow = head;
+    let fast = head.next;
+    
+    while(fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    
+//     Reverse the second half
+    let next = slow.next;
+    let prev = null;
+    
+    slow.next = fast;
+    slow = next;
+    
+    while(slow) {
+        next = slow.next;
+        slow.next = prev;
+        prev = slow;
+        slow = next;
+    }
+
+//     Create another pointer to add and store max
+    let newSlow = head;
+    let max = 0;
+    while(fast) {
+        console.log(newSlow.val, fast.val);
+        max = Math.max(newSlow.val + fast.val, max);
+        newSlow = newSlow.next;
+        fast = fast.next;
+    }
+    
+    console.log(max);
+    return max;
+};
