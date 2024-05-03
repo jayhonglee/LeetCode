466ec3b326a5b840e3b362fc1aa573a763363167
Jaehong Lee <contact@jaehonglee.com>
2024-05-03 12:16:15 -0700

    Time: 674 ms (18.62%), Space: 81.5 MB (64.53%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function(nums, k) {
+//     declare variables
+    let queue = [], ans = [];
+    
+//     loop over the nums array
+    for(let i = 0; i < nums.length; i++) {
+        
+//          if the current num is bigger than the queue[0] empty the queue
+        while(queue && nums[i] > nums[queue[queue.length - 1]]) {
+            queue.pop();
+        }
+//          enqueue current num to the queue
+        queue.push(i);
+        
+//          if the queue has elements bigger than k, dequeue the first element
+        while(queue[0] < i - (k - 1)) {
+            queue.shift();
+        }
+        
+//          if the index is bigger than or equal to k-1 store the queue[0] to the answer array
+        if(i >= k - 1) {
+            ans.push(nums[queue[0]]);
+        }
+        
+    }
+    
+    return ans;
+};
