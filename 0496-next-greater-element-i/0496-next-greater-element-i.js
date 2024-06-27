6c819f34a577e8578196b157fd58b393718e3d9c
Jaehong Lee <contact@jaehonglee.com>
2024-06-26 22:16:50 -0700

    Time: 60 ms (70.27%), Space: 52.5 MB (14.37%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -4,27 +4,30 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-    let ans = [];
+//     notes
+//     nums1 = list of numbers to care for
+//     nums2 = array of interest
     
-//     loop over nums1 array
-    nums1.forEach((x)=> {
-        //     for each element loop over nums 2 
-        let isXFound = false, foundY;
-        for(const y of nums2) {
-            //       if a greater element is found return it to the ans array
-            if(isXFound && y > x) {
-                foundY = y;
-                isXFound = false;
-                continue;
-            }
-            //      if the same element is found set start
-            if(!isXFound && x === y) isXFound = true;
+//  declare variables
+    let stack = [], hashmap = new Map(), ans = [];
+    
+//  iterate over num2 and store the next bigger element in a hashmap
+    for(const y of nums2) {
+        while(stack[stack.length-1] < y) {
+            hashmap.set(stack.pop(), y);
         }
-        return foundY ? ans.push(foundY) : ans.push(-1);
-        
-    })
+        stack.push(y);
+    }
+    
+// for the reamining elements in the stack, set them to -1 in hahsmap
+    for(const z of stack) {
+        hashmap.set(z, -1);
+    }
+    
+// iterate over nums1 and store corresponding value to ans array
+    for(const x of nums1) {
+        ans.push(hashmap.get(x));
+    }
     
     return ans;
-
-
 };
