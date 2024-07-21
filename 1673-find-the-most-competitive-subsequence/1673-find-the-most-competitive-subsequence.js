1639c2ea7d7ed8ecbc24ac6e660c721adcffb166
Jaehong Lee <contact@jaehonglee.com>
2024-07-20 19:13:06 -0700

    Time: 175 ms (48.48%), Space: 76.5 MB (78.79%) - LeetHub

--- 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.js
+++ 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.js
@@ -12,18 +12,19 @@ var mostCompetitive = function(nums, k) {
 //     Else if the number is bigger and remaining number of numbers is available then push
 //     Else if the number is smaller (while) pop the stack and push it
     for(let i = 0; i < nums.length; i++) {
+        const currentNum = nums[i];
         const stackLen = stack.length;
         const stackLenMinusOne = stackLen - 1;
         
-        if(k - stackLen === nums.length - i) stack.push(nums[i]);
-        else if(nums[i] >= stack[stackLenMinusOne] && stackLen < k || !stackLen) stack.push(nums[i]);
-        else if(nums[i] < stack[stackLenMinusOne]) {
+        if(k - stackLen === nums.length - i) stack.push(currentNum);
+        else if(currentNum >= stack[stackLenMinusOne] && stackLen < k || !stackLen) stack.push(nums[i]);
+        else if(currentNum < stack[stackLenMinusOne]) {
             let run = false;
-            while(nums[i] < stack[stack.length - 1] && (stack.length - 1) + (nums.length - i) >= k) {
+            while(currentNum < stack[stack.length - 1] && (stack.length - 1) + (nums.length - i) >= k) {
                 stack.pop();
                 run = true;
             }
-            if(run) stack.push(nums[i]);
+            if(run) stack.push(currentNum);
         }
     }
 
