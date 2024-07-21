1b84f1afcbb316f785d1733b6d5d0b8cc91ab806
Jaehong Lee <contact@jaehonglee.com>
2024-07-20 19:12:06 -0700

    Time: 151 ms (90.91%), Space: 77.1 MB (69.70%) - LeetHub

--- 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.js
+++ 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.js
@@ -12,9 +12,12 @@ var mostCompetitive = function(nums, k) {
 //     Else if the number is bigger and remaining number of numbers is available then push
 //     Else if the number is smaller (while) pop the stack and push it
     for(let i = 0; i < nums.length; i++) {
-        if(k - stack.length === nums.length - i) stack.push(nums[i]);
-        else if(nums[i] >= stack[stack.length - 1] && stack.length < k || !stack.length) stack.push(nums[i]);
-        else if(nums[i] < stack[stack.length - 1]) {
+        const stackLen = stack.length;
+        const stackLenMinusOne = stackLen - 1;
+        
+        if(k - stackLen === nums.length - i) stack.push(nums[i]);
+        else if(nums[i] >= stack[stackLenMinusOne] && stackLen < k || !stackLen) stack.push(nums[i]);
+        else if(nums[i] < stack[stackLenMinusOne]) {
             let run = false;
             while(nums[i] < stack[stack.length - 1] && (stack.length - 1) + (nums.length - i) >= k) {
                 stack.pop();
