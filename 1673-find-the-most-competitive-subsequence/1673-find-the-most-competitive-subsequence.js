017800ff9e41210545a289fd49d0fe26a017d996
Jaehong Lee <contact@jaehonglee.com>
2024-07-20 19:09:07 -0700

    Time: 151 ms (90.91%), Space: 77.2 MB (66.67%) - LeetHub

---
+++ 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var mostCompetitive = function(nums, k) {
+//     Define variables
+    let stack = [];
+    
+//     Loop over the nums array 
+//     If remaining elements in nums is equal to remaining number of numbers for result, push it in
+//     Else if the number is bigger and remaining number of numbers is available then push
+//     Else if the number is smaller (while) pop the stack and push it
+    for(let i = 0; i < nums.length; i++) {
+        if(k - stack.length === nums.length - i) stack.push(nums[i]);
+        else if(nums[i] >= stack[stack.length - 1] && stack.length < k || !stack.length) stack.push(nums[i]);
+        else if(nums[i] < stack[stack.length - 1]) {
+            let run = false;
+            while(nums[i] < stack[stack.length - 1] && (stack.length - 1) + (nums.length - i) >= k) {
+                stack.pop();
+                run = true;
+            }
+            if(run) stack.push(nums[i]);
+        }
+    }
+
+    return stack;
+};
