6ad58dd04ab1b363edb525e4bcfc0cea8f0182cd
Jaehong Lee <contact@jaehonglee.com>
2024-08-03 13:46:53 -0700

    Time: 50 ms (73.92%), Space: 49.2 MB (51.75%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -14,8 +14,7 @@
 var isSameTree = function(p, q) {
 //     base case: if p & q are both null return true if p or q is null but the other is not return false
     if(p === null && q === null) return true;
-    if(p === null && q != null) return false;
-    if(p != null && q === null) return false;
+    if(p === null || q === null) return false;
     
 //     isSame when p.val = q.val && p.left = q.left && p.right = q.right
     return p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
