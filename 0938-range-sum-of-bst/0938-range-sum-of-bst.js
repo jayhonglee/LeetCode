10dfe6dc7f35ae9b6a0cda2db595d930cfe31088
Jaehong Lee <contact@jaehonglee.com>
2024-08-18 14:49:53 -0700

    Time: 171 ms (47.64%), Space: 80.9 MB (35.84%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.js
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.js
@@ -13,12 +13,12 @@
  * @return {number}
  */
 var rangeSumBST = function(root, low, high) {
-  if(!root) return;   
+  if(!root) return 0;   
     
   let ans = 0;  
   if(root.val >= low && root.val <= high) ans += root.val;
-  if(root.left) ans += rangeSumBST(root.left, low, high);
-  if(root.right) ans += rangeSumBST(root.right, low, high);  
+  if(root.val > low) ans += rangeSumBST(root.left, low, high);
+  if(root.val < high) ans += rangeSumBST(root.right, low, high);  
     
   return ans;
 };
