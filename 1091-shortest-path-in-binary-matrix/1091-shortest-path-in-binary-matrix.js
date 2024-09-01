71dd4b6346fc0d29537ed7016ced872c5ce86fc3
Jaehong Lee <contact@jaehonglee.com>
2024-09-01 13:35:56 -0700

    Time: 186 ms (31.32%), Space: 62.4 MB (30.00%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.js
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.js
@@ -3,32 +3,23 @@
  * @return {number}
  */
 var shortestPathBinaryMatrix = function(grid) {
-    let ans = -1;
+    if(grid[0][0] != 0) return -1;
     
-    if(grid[0][0] != 0) return ans;
-    
-    const format = (row, col) => {
-        return row + "," + col;
-    }
-    
-    let seen = new Set();
+    let queue = [[0,0]], nextQueue = [], seen = new Set(), ans = 1;
     let directions = [[0, 1], [1, 0], [1, 1], [-1, -1], [-1, 1], [1, -1], [0, -1], [-1, 0]];
-    let queue = [[0,0]], nextQueue = [];
-    
+
+    const format = (row, col) => row + "," + col;
+
     const valid = (row, column) => {
-        if(row < 0 || column < 0 || row >= grid.length || column >= grid[0].length || grid[row][column] !== 0) return false;
-        return true;
+        return (row < 0 || column < 0 || row >= grid.length || column >= grid[0].length || grid[row][column] !== 0) ? false : true;
     }
     
-    ans = 1;
-    while(queue.length > 0) {
-        nextQueue = [];
-        
+    while(queue.length > 0) {        
         for(const [row, col] of queue) {
             if(row === grid.length - 1 && col === grid[0].length - 1) return ans;
             
             seen.add(format(row,col));
-            
+        
             for(const [x, y] of directions) {
                 if(valid(row + x, col + y) && !seen.has(format(row + x, col + y))) {
                     nextQueue.push([row + x, col + y]);
@@ -38,6 +29,7 @@ var shortestPathBinaryMatrix = function(grid) {
         }
         
         queue = nextQueue;
+        nextQueue = [];
         ans++;
     }
     
