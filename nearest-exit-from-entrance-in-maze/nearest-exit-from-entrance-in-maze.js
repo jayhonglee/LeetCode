5b4c79c577ea3c5592c90d7b8edf3bd5cb1556fd
Jaehong Lee <contact@jaehonglee.com>
2024-09-10 05:01:44 -0700

    Time: 160 ms, Memory: 65.9 MB - LeetHub

--- nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.js
+++ nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.js
@@ -6,17 +6,17 @@
 var nearestExit = function(maze, entrance) {
 //     bfs: returns the level when the nearest exit is found
 //     perform bfs at entrance
-    const format = (y,x) => x + ',' + y;
-    const outOfBounds = (y,x) => x < 0 || x > maze[0].length - 1 || y < 0 || y > maze.length -1;
-    const isBoundary = (y,x) => x === 0 || x === maze[0].length -1 || y === 0 || y === maze.length - 1;
-    const isEmpty = (y,x) => maze[y][x] === ".";
+    const format = (x,y) => x + ',' + y;
+    const outOfBounds = (x,y) => x < 0 || x > maze[0].length - 1 || y < 0 || y > maze.length -1;
+    const isBoundary = (x,y) => x === 0 || x === maze[0].length -1 || y === 0 || y === maze.length - 1;
+    const isEmpty = (x,y) => maze[y][x] === ".";
     let queue = [entrance], nextQueue = [], seen = new Set(), dirs = [[0,1],[0,-1],[1,0],[-1,0]], level = 0;
     
     while(queue.length) {
         for(const [row, col] of queue) {
-            if(seen.has(format(row,col)) || outOfBounds(row, col) || !isEmpty(row,col)) continue;
+            if(seen.has(format(col,row)) || outOfBounds(col, row) || !isEmpty(col,row)) continue;
             
-            if(isBoundary(row, col) && isEmpty(row, col) && format(entrance[0], entrance[1]) !== format(row, col)) {
+            if(isBoundary(col, row) && isEmpty(col, row) && format(entrance[1], entrance[0]) !== format(col, row)) {
                 return level;
             }
             
@@ -24,7 +24,7 @@ var nearestExit = function(maze, entrance) {
                 nextQueue.push([row+x, col+y]);
             }
             
-            seen.add(format(row, col));
+            seen.add(format(col, row));
         }
         
         queue = nextQueue;
