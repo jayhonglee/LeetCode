9398795aa663beb9d2c5ef0a5f69a330dfde1505
Jaehong Lee <contact@jaehonglee.com>
2024-10-11 07:21:19 -0700

    Time: 58 ms (67.81%), Space: 51.1 MB (96.97%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -10,9 +10,7 @@ var fizzBuzz = function(n) {
         const byThree = i % 3 == 0;
         const byFive = i % 5 == 0;
         
-        console.log(i, byFifteen, byThree, byFive);
-        
-        if(byFifteen) ans.push('FizzBuzz');
+        if(byThree && byFive) ans.push('FizzBuzz');
         else if (byThree) ans.push('Fizz');
         else if (byFive) ans.push('Buzz');
         else ans.push(`${i}`);
