fad1a0b04c44f69593130453bcdce5ea4be7d0e2
Jaehong Lee <contact@jaehonglee.com>
2024-10-14 09:08:43 -0700

    Time: 63 ms (12.00%), Space: 50.4 MB (84.17%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -0,0 +1,28 @@
+class Solution {
+    public double getDistance(int[] coor) {
+        return Math.sqrt(Math.pow(coor[0], 2) + Math.pow(coor[1], 2)); 
+    }
+    
+    public int[][] kClosest(int[][] points, int k) {
+//         Define a min heap
+        PriorityQueue<int[]> heap = new PriorityQueue<>((n1, n2) -> {
+            return Double.compare(getDistance(n2), getDistance(n1));
+        });
+        
+//         Loop over points and push it into heap
+//         If the size goes over k pop
+        for(int[] point : points) {
+            heap.add(point);
+            if(heap.size() > k) heap.remove();
+        }
+        
+//         Return min heap as int[][]
+        int[][] ans = new int[k][2];
+        int index = 0;
+        while(!heap.isEmpty()) {
+            ans[index++] = heap.poll();
+        }
+        
+        return ans;
+    }
+}
