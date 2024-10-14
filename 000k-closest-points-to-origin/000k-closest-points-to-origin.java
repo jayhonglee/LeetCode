49f3b7d504009dab1fb20aa904fd850800b9282d
Jaehong Lee <contact@jaehonglee.com>
2024-10-14 08:54:45 -0700

    Time: 62 ms, Memory: 50.5 MB - LeetHub

--- 000k-closest-points-to-origin/000k-closest-points-to-origin.java
+++ 000k-closest-points-to-origin/000k-closest-points-to-origin.java
@@ -1,29 +1,26 @@
-import java.util.PriorityQueue;
-
 class Solution {
     public double getDistance(int[] coor) {
         return Math.sqrt(Math.pow(coor[0], 2) + Math.pow(coor[1], 2)); 
     }
     
     public int[][] kClosest(int[][] points, int k) {
-        // Define a max heap (reverse order comparator)
-        PriorityQueue<int[]> heap = new PriorityQueue<>((n1, n2) -> 
-            Double.compare(getDistance(n2), getDistance(n1))
-        );
+//         Define a min heap
+        PriorityQueue<int[]> heap = new PriorityQueue<>((n1, n2) -> {
+            if(getDistance(n1) > getDistance(n2)) return -1;
+            else return 1;
+        });
         
-        // Loop over points and push them into the heap
-        // If the size goes over k, remove the largest distance point
-        for (int[] point : points) {
+//         Loop over points and push it into heap
+//         If the size goes over k pop
+        for(int[] point : points) {
             heap.add(point);
-            if (heap.size() > k) {
-                heap.remove();
-            }
+            if(heap.size() > k) heap.remove();
         }
         
-        // Return the k closest points as an int[][]
+//         Return min heap as int[][]
         int[][] ans = new int[k][2];
         int index = 0;
-        while (!heap.isEmpty()) {
+        while(!heap.isEmpty()) {
             ans[index++] = heap.poll();
         }
         
