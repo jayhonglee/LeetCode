998ca601a6a6e9ca692d68e63614b2417aa28492
Jaehong Lee <contact@jaehonglee.com>
2024-10-15 07:17:00 -0700

    Time: 236 ms (84.67%), Space: 78.1 MB (63.33%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.js
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.js
@@ -10,12 +10,14 @@ var successfulPairs = function(spells, potions, success) {
     const j = potions.length;
     let ans = [];
     
-   let binarySearch = (arr, target) => {
-        left = 0;
-        right = arr.length - 1;
-        while (left <= right) {
+    const binarySearch = (arr, tar) => {
+        let left = 0;
+        let right = arr.length - 1;
+        
+        while(left <= right) {
             let mid = Math.floor((left + right) / 2);
-            if (arr[mid] < target) {
+            
+            if (arr[mid] < tar) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
@@ -35,28 +37,3 @@ var successfulPairs = function(spells, potions, success) {
     return ans;
 };
 
-//    let binarySearch = (arr, target) => {
-//         left = 0;
-//         right = arr.length - 1;
-//         while (left <= right) {
-//             let mid = Math.floor((left + right) / 2);
-//             if (arr[mid] < target) {
-//                 left = mid + 1;
-//             } else {
-//                 right = mid - 1;
-//             }
-//         }
-        
-//         return left;
-//     }
-    
-//     potions.sort((a, b) => a - b);
-//     let ans = [];
-//     let m = potions.length;
-    
-//     for (const spell of spells) {
-//         let i = binarySearch(potions, success / spell);
-//         ans.push(m - i);
-//     }
-    
-//     return ans;
