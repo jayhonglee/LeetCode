e66d3aaf4477cd9867ec05028fb6721a80bd062a
Jaehong Lee <contact@jaehonglee.com>
2024-11-21 13:51:08 -0800

    Time: 5 ms, Memory: 50.9 MB - LeetHub

---
+++ generate-parentheses/generate-parentheses.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    const backtrack = (current) => {
+        if(right > left || left > n) return;
+        if(current.length === n * 2 && left === right) ans.push(current);
+        if(current.length > n * 2) return;
+        
+        for(const char of ["(", ")"]) {
+            if(char === "(") left++;
+            else right++;
+            
+            backtrack(current + char);
+            
+            if(char === "(") left--;
+            else right--;
+        }
+    }    
+    
+    let left = 0, right = 0, ans = [];
+    backtrack("");
+    
+    return ans;
+};
+
