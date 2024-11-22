ac6a7ffd9f57b102c5e1368b7ae2a607836b9730
Jaehong Lee <contact@jaehonglee.com>
2024-11-22 13:06:17 -0800

    Time: 158 ms (14.32%), Space: 50.9 MB (49.80%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -6,33 +6,26 @@ var lengthOfLIS = function(nums) {
 //     create a dp function
 //     input: index (this will be the index of the last element that is sti)
 //     output: length of longest sti
-    const dp = function(index) {
-        if(index === 0) {
-            memo[0] = 1;
-            return memo[0];
-        }
+    const dp = function(index) {        
+        if(memo[index] !== -1) return memo[index];
         
-        if(memo[index] !== -1) {
-            return memo[index];
-        }
-        
-        let longestFound = 1;
+        let ans = 1;
         for(let i = 0; i < index; i++) {
             if(nums[i] < nums[index]) {
-                longestFound = Math.max(longestFound, dp(i) + 1);
+                ans = Math.max(ans, dp(i) + 1);
             }
         }
         
-        memo[index] = longestFound;
+        memo[index] = ans;
         return memo[index];
     }
     
 //     call dp for every index
-    const memo = new Array(nums.length).fill(-1);
+    let memo = new Array(nums.length).fill(-1);
+    let ans = 0;
     for(let i = 0; i < nums.length; i++) {
-        dp(i);
+        ans = Math.max(ans, dp(i));
     }
     
-//     return longest sti from the memo
-    return Math.max(...memo);
+    return ans;
 };
