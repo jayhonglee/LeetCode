3f8d80d43803afaeb475da80e7a0fd5fd2047d8c
Jaehong Lee <contact@jaehonglee.com>
2024-11-26 21:41:23 -0800

    Time: 0 ms (100.00%), Space: 49.2 MB (32.84%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -15,8 +15,6 @@ var findMin = function(nums) {
         const mid = Math.floor((left + right) / 2);
         const midNum = nums[mid];
         
-        console.log(mid, midNum)
-        
         if(nums[mid] > nums[mid + 1]) return nums[mid + 1];
         else if(nums[mid] < nums[mid - 1]) return nums[mid];
         
