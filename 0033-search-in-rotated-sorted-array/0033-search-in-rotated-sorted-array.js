b80a4441e6564b4732152fd58a82af5c7112137e
Jaehong Lee <contact@jaehonglee.com>
2024-11-26 22:25:17 -0800

    Time: 0 ms (100.00%), Space: 49.4 MB (19.00%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,50 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    const binarySearch = function(left, right) {
+        while(left <= right) {
+            const mid = Math.floor((left + right) / 2);
+
+            if(nums[mid] === target) return mid;
+
+            if(nums[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+
+        return -1;
+    }
+    
+//     edge case
+    if(nums[0] < nums[nums.length - 1]) return binarySearch(0, nums.length - 1);
+    if(nums.length === 1 && nums[0] === target) return 0;
+
+    // 51234 45123 34512 23451
+//     find pivot point
+    let pivot = -1;
+    let left = 0, right = nums.length - 1;
+    while(left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if(nums[mid] > nums[mid + 1]) {
+            pivot = mid + 1;
+            break;
+        }
+        if(nums[mid - 1] > nums[mid]) {
+            pivot = mid;
+            break;
+        }
+
+        if(nums[mid] < nums[0]) right = mid - 1;
+        else left = mid + 1;
+    }
+    
+//     bs for left array and right array
+    let answer = binarySearch(0, pivot - 1);
+    
+    if (answer != -1) return answer;
+    
+    return binarySearch(pivot, nums.length - 1);
+};
