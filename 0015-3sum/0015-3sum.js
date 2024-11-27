3a1c1d8f1e4209940ed367dee6f1f5d1b6520be6
Jaehong Lee <contact@jaehonglee.com>
2024-11-27 07:08:00 -0800

    Time: 563 ms (8.91%), Space: 68.7 MB (5.38%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,31 +3,27 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-//     sort the array
-//     basically for every num of nums do two sum apporach to find num pairs that will be added with current num to add up to 0. keep a seen set to skip same nums.
-    
-    nums.sort();
-    const seenI = new Set();
-    let ans = [];
+    let dup = new Set();
+    let ans = new Set();
     
     for(let i = 0; i < nums.length; i++) {
-        if(seenI.has(nums[i])) continue;
-        seenI.add(nums[i]);
+        const ithNum = nums[i];
         
-        const targetSum = 0 - nums[i];
-        const map = new Map();
-        const seenJ = new Set();
+        if(dup.has(ithNum)) continue;
+        dup.add(ithNum);
+        
+        const target = 0 - ithNum;
+        
+        let seen = new Map();
         for(let j = i + 1; j < nums.length; j++) {
-            if(map.has(targetSum - nums[j])) seenJ.add(`${nums[i]},${targetSum - nums[j]},${nums[j]}`);
+            const complement = target - nums[j];
+            if(seen.has(complement)) {
+                ans.add(JSON.stringify([ithNum, nums[j], complement].sort()));
+            }
             
-            if(!map.has(nums[j])) map.set(nums[j], 0);
-            map.set(nums[j], map.get(nums[j]) + 1);
+            seen.set(nums[j], j);
         }
-        
-        seenJ.forEach((seen) => {
-            ans.push(seen.split(",").map((str) => +str));
-        })
     }
     
-    return ans;
+    return Array.from(ans, JSON.parse);
 };
