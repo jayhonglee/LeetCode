58b0a63bb711278f4c562bb650b6ca525676c58c
Jaehong Lee <contact@jaehonglee.com>
2024-11-28 11:16:59 -0800

    Time: 0 ms (100.00%), Space: 48.6 MB (92.45%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,18 +6,14 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    
 // loop over nums 1 array from right to left i 
 //     have two pointers pointing to the end of nums1 & end of nums2 (n - 1) j k
 //     for every i, compare j and k values and store the bigger one to i 
 //      then update j or k pointers accordingly - 1
     let j = m - 1, k = n - 1;
     for(let i = m + n - 1; i >= 0; i--) {
-        if (k < 0) {
-            break;
-        }
         
-        if(nums1[j] >= nums2[k]) {
+        if(nums1[j] >= nums2[k] || k < 0) {
             nums1[i] = nums1[j];
             j--;
         } else {
