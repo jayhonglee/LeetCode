74e91f0a723e42039217deb413852bb6d0d8b9a7
Jaehong Lee <contact@jaehonglee.com>
2024-11-29 10:42:27 -0800

    Time: 1 ms (82.34%), Space: 51 MB (59.91%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.js
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -3,14 +3,30 @@
  * @return {number}
  */
 var jump = function(nums) {
-    let memo = new Array(nums.length).fill(Infinity);
-    memo[nums.length - 1] = 0;
+//     dp top down:
+//     let memo = new Array(nums.length).fill(Infinity);
+//     memo[nums.length - 1] = 0;
     
-    for(let index = nums.length - 2; index >= 0; index--) {      
-        for(let i = 1; i <= nums[index]; i++) {
-            memo[index] = Math.min(memo[Math.min(index + i, nums.length - 1)] + 1, memo[index]);
+//     for(let index = nums.length - 2; index >= 0; index--) {      
+//         for(let i = 1; i <= nums[index]; i++) {
+//             memo[index] = Math.min(memo[Math.min(index + i, nums.length - 1)] + 1, memo[index]);
+//         }
+//     }
+
+//     return memo[0];
+    
+//     greedy:
+    let currEnd = 0;
+    let farthest = 0;
+    let ans = 0;
+    for (let index = 0; index < nums.length - 1; index++) {
+        farthest = Math.max(farthest, index + nums[index]);
+        
+        if(index === currEnd) {
+            ans++;
+            currEnd = farthest;
         }
     }
-
-    return memo[0];
+    
+    return ans;
 };
