3929d392ea96afffade3cf3af51707a6189241fc
Jaehong Lee <contact@jaehonglee.com>
2024-11-29 14:57:32 -0800

    Time: 1 ms (78.72%), Space: 49.3 MB (59.09%) - LeetHub

--- 0274-h-index/0274-h-index.js
+++ 0274-h-index/0274-h-index.js
@@ -3,22 +3,34 @@
  * @return {number}
  */
 var hIndex = function(citations) {
-//     brute force:
-    let hashmap = new Map();
-    for(const citation of citations) {
-        for(let i = 0; i <= citation; i++) {
-            if(!hashmap.has(i)) hashmap.set(i, 0);
+//     brute force: O(n^2)
+//     let hashmap = new Map();
+//     for(const citation of citations) {
+//         for(let i = 0; i <= citation; i++) {
+//             if(!hashmap.has(i)) hashmap.set(i, 0);
             
-            const newValue = hashmap.get(i) + 1;
-            hashmap.set(i, newValue);
-        }
+//             const newValue = hashmap.get(i) + 1;
+//             hashmap.set(i, newValue);
+//         }
+//     }
+    
+//     hashmap = [...hashmap].sort((a,b) => {
+//         return b[0] - a[0];
+//     })
+    
+//     for(const [key, value] of hashmap) {
+//         if(key <= value) return key;
+//     }
+    
+//     O(n) attempt:
+    citations.sort((a,b) => b - a);
+    
+    let ans = 0;
+    for(let i = 0; i <= citations.length - 1; i++) {
+        const curr = citations[i];
+        
+        if(i + 1 <= curr) ans = Math.max(ans, i + 1);
     }
     
-    hashmap = [...hashmap].sort((a,b) => {
-        return b[0] - a[0];
-    })
-    
-    for(const [key, value] of hashmap) {
-        if(key <= value) return key;
-    }
+    return ans;
 };
