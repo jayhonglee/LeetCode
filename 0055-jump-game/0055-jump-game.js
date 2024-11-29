82f9e4bd4a6834e591daab2d32368343b3192747
Jaehong Lee <contact@jaehonglee.com>
2024-11-28 22:18:14 -0800

    Time: 880 ms (6.42%), Space: 55.3 MB (13.60%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    const backtrack = function(index) {
+        if(index >= nums.length - 1) return true;
+        
+        if(memo[index] === "bad") return false;
+        
+        for(let i = 1; i <= nums[index]; i++) {
+            if(backtrack(Math.min(index + i, nums.length - 1)) === true) return true;
+        }
+        memo[index] = "bad";
+        
+        return false;
+    }
+    
+    const memo = new Array(nums.length).fill("unknown");
+    return backtrack(0);
+};
