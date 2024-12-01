553564233d0a73b6dc3fd4c08d6dc95008a64fcc
Jaehong Lee <contact@jaehonglee.com>
2024-11-30 19:18:51 -0800

    Time: 14 ms (13.11%), Space: 59.3 MB (5.02%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -3,34 +3,32 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    const map = {
-        I: 1,
-        IV: 4,
-        V: 5,
-        IX: 9,
-        X: 10,
-        XL: 40,
-        L: 50,
-        XC: 90,
-        C: 100,
-        CD: 400,
-        D: 500,
-        CM: 900,
-        M: 1000
-    }
+    const hashmap = new Map([
+        ["I", 1],
+        ["IV", 4],
+        ["V", 5],
+        ["IX", 9],
+        ["X", 10],
+        ["XL", 40],
+        ["L", 50],
+        ["XC", 90],
+        ["C", 100],
+        ["CD", 400],
+        ["D", 500],
+        ["CM", 900],
+        ["M", 1000],
+    ])
     
     let ans = 0;
     for(let i = s.length - 1; i >= 0; i--) {
         const currentNumeral = s[i];
         const currentWithPrevNumeral = s[i - 1] + s[i];
         
-        if(map[currentWithPrevNumeral]) {
-            console.log(i,map[currentWithPrevNumeral])
-            ans += map[currentWithPrevNumeral];
+        if(hashmap.has(currentWithPrevNumeral)) {
+            ans += hashmap.get(currentWithPrevNumeral);
             i = i - 1;
         } else {
-            console.log(i,map[currentNumeral])
-            ans += map[currentNumeral];
+            ans += hashmap.get(currentNumeral);
         }
     }
     
