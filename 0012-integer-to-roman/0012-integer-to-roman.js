c6817a88f7f8eec8bacf19aac977a39b9657255e
Jaehong Lee <contact@jaehonglee.com>
2024-11-30 19:51:04 -0800

    Time: 17 ms (13.99%), Space: 57.9 MB (17.44%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.js
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -4,7 +4,7 @@
  */
 var intToRoman = function(num) {
     num = num.toString();
-    
+
     const hashmap = new Map([
         [1, "I"],
         [4, "IV"],
@@ -20,30 +20,30 @@ var intToRoman = function(num) {
         [900, "CM"],
         [1000, "M"],
     ])
-    
+
     let ans = [];
-    for(let i = 0; i < num.length; i++) {
-        const currentNum = num[i] * (10 ** (num.length - (i+1)));
+    for (let i = 0; i < num.length; i++) {
+        const currentNum = num[i] * (10 ** (num.length - (i + 1)));
         const currentNumStr = currentNum.toString();
         const biggestDigit = currentNumStr[0];
         const remainingDigits = currentNumStr.length - 1;
-        
-        if(hashmap.has(currentNum)) {
+
+        if (hashmap.has(currentNum)) {
             ans.push(hashmap.get(currentNum));
         } else {
-            if(biggestDigit > 5) {
+            if (biggestDigit > 5) {
                 ans.push(hashmap.get(5 * (10 ** remainingDigits)));
-                
-                for(let i = 0; i < biggestDigit - 5; i++) {
+
+                for (let i = 0; i < biggestDigit - 5; i++) {
                     ans.push(hashmap.get(10 ** remainingDigits));
                 }
             } else {
-                for(let i = 0; i < biggestDigit; i++) {
+                for (let i = 0; i < biggestDigit; i++) {
                     ans.push(hashmap.get(10 ** remainingDigits));
                 }
             }
         }
     }
-    
+
     return ans.join("");
 };
