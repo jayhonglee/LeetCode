5e9c81e8cdc76afcc2e1970aa83b8ace5a0d10a0
Jaehong Lee <contact@jaehonglee.com>
2024-12-01 20:18:35 -0800

    Time: 7 ms (13.79%), Space: 51.7 MB (14.61%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+//     iterate over each character
+//     if its not empty add it word
+//     if its empty store current word to array and set word to new empty string
+    let word = "", words = [];
+    let left = 0, right = s.length - 1;
+    
+    while(left <= right && s[left] === " ") left++;
+    while(left <= right && s[right] === " ") right--;
+    
+    for(let i = left; i <= right; i++) {
+        if(s[i] === " " && word.length !== 0) {
+            words.push(word);
+            word = "";
+        } else if(s[i] !== " ") {
+            word += s[i];
+            if(i === right && word.length !== 0) words.push(word);
+        }
+    }
+
+    return words.reverse().join(" ");
+};
