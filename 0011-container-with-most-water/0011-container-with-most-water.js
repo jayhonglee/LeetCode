4f9f6d32429edb3a449f16e3c6bb4cd194218ca2
Jaehong Lee <contact@jaehonglee.com>
2024-12-02 16:52:19 -0800

    Time: 1 ms (95.77%), Space: 57.7 MB (53.66%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -3,19 +3,17 @@
  * @return {number}
  */
 var maxArea = function(height) {
-//     optimal solution
-//     using two pointers
-//     start from first and last elements
-//     and find the shorter line between first and last and update them inward
-    let left = 0, right = height.length - 1;
+    let left = 0
+    let right = height.length - 1;
     let ans = 0;
     
     while(left < right) {
-        const w = right - left;
-        const h = Math.min(height[left], height[right]);
-        ans = Math.max(ans, w * h);
+        const leftHeight = height[left];
+        const rightHeight = height[right];
+        const area = Math.min(leftHeight, rightHeight) * (right - left);
+        ans = Math.max(ans, area);
         
-        if(height[left] > height[right]) {
+        if(leftHeight >= rightHeight) {
             right--;
         } else {
             left++;
@@ -23,24 +21,4 @@ var maxArea = function(height) {
     }
     
     return ans;
-//     brute force:
-//     iterate over the heights and store to hashmap (index, height)
-//     for each height check the maximum area and compare it with ans (Math.max(ans, max))
-    
-//     const hashmap = new Map();
-//     let ans = -1;
-    
-//     for(let i = 0; i < height.length; i++) {
-//         const current = height[i] ;
-//         let currentBarBiggest = 0;
-        
-//         hashmap.forEach((value, key) => {
-//             currentBarBiggest = Math.max((i - key) * Math.min(current, value), currentBarBiggest);
-//         })
-        
-//         hashmap.set(i, current);
-//         ans = Math.max(ans, currentBarBiggest);
-//     }
-    
-//     return ans;
 };
