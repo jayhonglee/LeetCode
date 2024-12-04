4fe2ae1c92ae6e102a7d737bbc7513cca1080ae9
Jaehong Lee <contact@jaehonglee.com>
2024-12-03 20:30:01 -0800

    Time: 2224 ms (22.32%), Space: 61.1 MB (13.82%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @param {string[]} words
+ * @return {number[]}
+ */
+var findSubstring = function(s, words) {
+    let dict = {};
+    for(const word of words) {
+        dict[word] = (dict[word] || 0) + 1;
+    }
+    
+    const wordCount = words.length;
+    const wordLength = words[0].length;
+    const totalStringLength = wordCount * wordLength;
+    let ans = [];
+    for(let i = 0; i < s.length - totalStringLength + 1; i++) {
+        const tempDict = {...dict};
+        let tempWordCount = 0;
+        
+        for(let j = i; j < i + totalStringLength; j += wordLength) {
+            const currWord = s.slice(j, j + wordLength);
+            
+            if(tempDict[currWord]) {
+                tempDict[currWord]--;
+                tempWordCount++;
+            }
+        }
+        
+        if(tempWordCount === wordCount) ans.push(i);
+    }
+    
+    return ans;
+};
