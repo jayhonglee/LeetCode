924bc1070e0b09a10653c647839212f89b69eec5
Jaehong Lee <contact@jaehonglee.com>
2024-12-04 22:41:29 -0800

    Time: 914 ms (40.60%), Space: 78.2 MB (12.07%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    const set = new Set();
+    for(const num of nums) {
+        set.add(num);
+    }
+    
+    let seen = new Set();
+    let ans = 0;
+    for(const num of set) {
+        if(!seen.has(num)) {
+            let currNum = num;
+            let count = 0;
+            while(!seen.has(currNum) && set.has(currNum)) {
+                console.log(num, currNum);
+                count++;
+                seen.add(currNum);
+                currNum = currNum + 1;
+                
+            }
+            currNum = num - 1;
+            while(!seen.has(currNum) && set.has(currNum)) {
+                console.log(num, currNum);
+                count++;
+                seen.add(currNum);
+                currNum = currNum - 1;
+                
+            }
+            
+            ans = Math.max(ans, count);
+        }
+    }
+    
+    return ans;
+};
