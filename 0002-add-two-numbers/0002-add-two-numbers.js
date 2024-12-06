20e7f067a3063a25c557f8457a73dbfcc335c4ac
Jaehong Lee <contact@jaehonglee.com>
2024-12-05 18:05:30 -0800

    Time: 2 ms (96.90%), Space: 56.5 MB (54.79%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let hasCarry = false;
+    let previousNode = null;
+    let head = null;
+    while(l1 || l2) {
+        const newNode = {
+            val: hasCarry ? 1 : 0,
+            next: null
+        }
+        
+        hasCarry = false;
+        if(previousNode) previousNode.next = newNode;
+        
+        newNode.val += (l1 ? l1.val : 0)
+        newNode.val += (l2 ? l2.val : 0);
+        
+        if(newNode.val >= 10) {
+            newNode.val %= 10;
+            hasCarry = true;
+        }
+        
+        l1 = l1 ? l1.next : l1;
+        l2 = l2 ? l2.next : l2;
+        previousNode = newNode;
+        if(!head) head = newNode;
+    }
+    
+    if(hasCarry) {
+        previousNode.next = {
+            val: 1,
+            next: null
+        }
+    }
+    
+    return head;
+};
