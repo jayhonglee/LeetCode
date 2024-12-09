08458f72f8fac519897c2d755d534d72595fffac
Jaehong Lee <contact@jaehonglee.com>
2024-12-08 20:57:16 -0800

    Time: 59 ms (35.54%), Space: 128.9 MB (5.88%) - LeetHub

---
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} inorder
+ * @param {number[]} postorder
+ * @return {TreeNode}
+ */
+var buildTree = function(inorder, postorder) {
+    if(!inorder.length || !postorder.length) return null;
+    
+//     find root = last element of postorder
+    const rootVal = postorder[postorder.length - 1];
+    const inorderRootIndex = inorder.indexOf(rootVal);
+    
+    const rootNode = new TreeNode(rootVal);
+    
+//     split inorder to left and right subtree
+    const inorderLeftSubtree = inorder.slice(0, inorderRootIndex);
+    const inorderRightSubtree = inorder.slice(inorderRootIndex + 1);
+    
+//     split postorder to left and right subtree
+//     left: using inorder left length from start
+//     right: using inorder right length from the rightmost (root);
+    const postorderLeftSubtree = postorder.slice(0, inorderLeftSubtree.length)
+    const postorderRightSubtree = postorder.slice(-inorderRightSubtree.length - 1, postorder.length - 1)
+    
+    rootNode.left = buildTree(inorderLeftSubtree, postorderLeftSubtree);
+    rootNode.right = buildTree(inorderRightSubtree, postorderRightSubtree);
+    
+    return rootNode;
+};
