29898f2bef72bcbf99d8fe457ab759673b530008
Jaehong Lee <contact@jaehonglee.com>
2024-12-08 21:04:41 -0800

    Time: 441 ms (5.07%), Space: 316.5 MB (5.88%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.js
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.js
@@ -12,26 +12,35 @@
  * @return {TreeNode}
  */
 var buildTree = function(inorder, postorder) {
-    if(!inorder.length || !postorder.length) return null;
+    const indexMap = new Map();
+    for(const [idx, node] of inorder.entries()) {
+        indexMap.set(node, idx);
+    }
     
-//     find root = last element of postorder
-    const rootVal = postorder[postorder.length - 1];
-    const inorderRootIndex = inorder.indexOf(rootVal);
+    const build = function() {
+        if(!inorder.length || !postorder.length) return null;
     
-    const rootNode = new TreeNode(rootVal);
-    
-//     split inorder to left and right subtree
-    const inorderLeftSubtree = inorder.slice(0, inorderRootIndex);
-    const inorderRightSubtree = inorder.slice(inorderRootIndex + 1);
-    
-//     split postorder to left and right subtree
-//     left: using inorder left length from start
-//     right: using inorder right length from the rightmost (root);
-    const postorderLeftSubtree = postorder.slice(0, inorderLeftSubtree.length)
-    const postorderRightSubtree = postorder.slice(-inorderRightSubtree.length - 1, postorder.length - 1)
-    
-    rootNode.left = buildTree(inorderLeftSubtree, postorderLeftSubtree);
-    rootNode.right = buildTree(inorderRightSubtree, postorderRightSubtree);
-    
-    return rootNode;
+    //     find root = last element of postorder
+        const rootVal = postorder[postorder.length - 1];
+        const inorderRootIndex = indexMap.get(rootVal);
+
+        const rootNode = new TreeNode(rootVal);
+
+    //     split inorder to left and right subtree
+        const inorderLeftSubtree = inorder.slice(0, inorderRootIndex);
+        const inorderRightSubtree = inorder.slice(inorderRootIndex + 1);
+
+    //     split postorder to left and right subtree
+    //     left: using inorder left length from start
+    //     right: using inorder right length from the rightmost (root);
+        const postorderLeftSubtree = postorder.slice(0, inorderLeftSubtree.length)
+        const postorderRightSubtree = postorder.slice(-inorderRightSubtree.length - 1, postorder.length - 1)
+
+        rootNode.left = buildTree(inorderLeftSubtree, postorderLeftSubtree);
+        rootNode.right = buildTree(inorderRightSubtree, postorderRightSubtree);
+
+        return rootNode;
+    }
+
+    return build();
 };
