639e1775abc9ae6dabcce4d9070fb91aeaa0671c
Jaehong Lee <contact@jaehonglee.com>
2024-12-25 23:04:13 -0800

    Time: 140 ms (16.13%), Space: 152.4 MB (7.61%) - LeetHub

--- 0077-combinations/0077-combinations.js
+++ 0077-combinations/0077-combinations.js
@@ -5,13 +5,11 @@
  */
 var combine = function(n, k) {
     const backtrack = (curr) => {
-        if(curr.length === k) return ans.push([...curr]);
+        if(curr.length === k) return ans.push(curr);
         const lastElement = curr[curr.length - 1] || 0;
         
         for(let i = lastElement + 1; i <= n; i++) {
-            curr.push(i);
-            backtrack(curr);
-            curr.pop();
+            backtrack([...curr, i]);
         }
     }
     
