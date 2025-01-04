d1603094f665c82f55467699f49116319a8ecf77
Jaehong Lee <contact@jaehonglee.com>
2025-01-03 21:32:33 -0800

    Time: 69 ms, Memory: 56.4 MB - LeetHub

---
+++ 19455-482-322-coin-change/19455-482-322-coin-change.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    let memo = new Array(amount + 1).fill(Infinity);
+    memo[0] = 0;
+
+    for(let i = 1; i <= amount; i++) {
+        for(const coin of coins) {
+            if(i - coin >= 0) {
+                memo[i] = Math.min(memo[i], memo[i - coin] + 1);
+            }
+        }
+    }
+
+    console.log(memo);
+    
+    return memo[amount] === Infinity ? -1 : memo[amount];
+    // const dp = (amount) => {
+    //     if (amount < 0) return Infinity; // Base case for invalid amount
+    //     if (memo[amount] !== Infinity) return memo[amount];
+
+    //     for (const coin of coins) {
+    //         memo[amount] = Math.min(memo[amount], dp(amount - coin) + 1);
+    //     }
+        
+    //     return memo[amount];
+    // };
+
+    // const ans = dp(amount);
+    // return ans === Infinity ? -1 : ans;
+};
