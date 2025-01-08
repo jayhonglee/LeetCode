a794d8d223f44d04bc077f85a3735551eefc7e93
Jaehong Lee <contact@jaehonglee.com>
2025-01-08 10:02:40 -0800

    Time: 12 ms, Memory: 57.6 MB - LeetHub

--- 22793-826-56-merge-intervals/22793-826-56-merge-intervals.js
+++ 22793-826-56-merge-intervals/22793-826-56-merge-intervals.js
@@ -5,16 +5,15 @@
 var merge = function(intervals) {
     intervals.sort((a,b) => a[0] - b[0]);
 
-    for(let i = 1; i < intervals.length; i++) {
-        const prev = intervals[i - 1];
+    let ans = [];
+    for(let i = 0; i < intervals.length; i++) {
+        const prev = ans[ans.length - 1];
         const curr = intervals[i];
 
-        if(prev[1] >= curr[0]) {
-            const merged = [prev[0], Math.max(prev[1], curr[1])];
-            intervals.splice(i - 1, 2, merged); 
-            i--;
-        }
+        if(ans.length && prev[1] >= curr[0]) {
+            prev[1] = Math.max(prev[1], curr[1]);
+        } else ans.push(curr);
     }
 
-    return intervals;
+    return ans;
 };
