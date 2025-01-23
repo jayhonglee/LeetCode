548b5aa9dcfef375571fb56038d6e987cd85adff
Jaehong Lee <contact@jaehonglee.com>
2025-01-23 09:39:31 -0800

    Time: 0 ms, Memory: 48.8 MB - LeetHub

---
+++ 10324-9927-50-powx-n/10324-9927-50-powx-n.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    if(n === 0) return 1;
+    if(n < 0) return 1 / myPow(x, -n);
+
+    if(n % 2 === 1) return x * myPow(x * x, (n - 1) / 2);
+    else return myPow(x * x, n / 2);
+};
