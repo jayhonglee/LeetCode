d101906142a041ccceb74697dc39e12acf01f769
Jaehong Lee <contact@jaehonglee.com>
2025-01-25 11:11:32 -0800

    Time: 122 ms, Memory: 71.4 MB - LeetHub

---
+++ 3204-69-286-walls-and-gates/3204-69-286-walls-and-gates.js
@@ -0,0 +1,52 @@
+/**
+ * @param {number[][]} rooms
+ * @return {void} Do not return anything, modify rooms in-place instead.
+ */
+var wallsAndGates = function(rooms) {
+    // Set variables
+    const rows = rooms.length;
+    const cols = rooms[0].length;
+    const dirs = [[0,1],[0,-1],[1,0],[-1,0]];
+    const seen = new Set();
+    const isValid = (row, col) => row >= 0 && col >= 0 && row < rows && col < cols;
+    const toCoordStr = (row, col) => row + "," + col;
+    let initialQueue = [];
+
+    // BFS: Using a replicate of rooms matrix, update each empty room value with the shortest distance value
+    const bfs = (queue) => {
+        let steps = 0;
+        let nextQueue = [];
+
+        while(queue.length) {
+            for(const [row, col] of queue) {
+                if(
+                    seen.has(toCoordStr(row, col)) || 
+                    !isValid(row, col) || 
+                    rooms[row][col] === -1
+                    ) continue;
+                seen.add(toCoordStr(row, col));
+                rooms[row][col] = Math.min(rooms[row][col], steps);
+
+                for(const [dy, dx] of dirs) {
+                    nextQueue.push([row + dy, col + dx]);
+                }
+            }
+
+            steps++;
+            queue = nextQueue;
+            nextQueue = [];
+        }
+        
+        seen.clear();
+    }
+    
+    // Loop over each rooms and if it's a gate add to initial queue
+    for(let row = 0; row < rows; row++) {
+        for(let col = 0; col < cols; col++) {
+            if(rooms[row][col] === 0) initialQueue.push([row,col]);
+        }
+    }
+
+    // Run BFS
+    bfs(initialQueue);
+};
