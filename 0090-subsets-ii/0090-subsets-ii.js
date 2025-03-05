0e482fa3e0f332aabf2524e58600ad48fed8cbbf
Jaehong Lee <contact@jaehonglee.com>
2025-03-05 11:32:05 -0800

    Time: 2 ms (48.56%), Space: 60.1 MB (5.49%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function(nums) {
+    let ans = [];
+
+    const backtrack = (curr, i) => {
+        ans.push([...curr]);
+
+        for(let idx = i; idx < nums.length; idx++) {
+            if (idx > i && nums[idx] === nums[idx - 1]) continue;
+
+            backtrack([...curr, nums[idx]], idx + 1);
+        }
+    }
+
+    nums.sort();
+    backtrack([], 0);
+
+    return ans;
+};
