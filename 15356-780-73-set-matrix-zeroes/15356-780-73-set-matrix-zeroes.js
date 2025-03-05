1eb5f1f2b43a16d8bd54c208ec185965a8a4e1a1
Jaehong Lee <contact@jaehonglee.com>
2025-03-04 16:14:29 -0800

    Time: 4 ms, Memory: 59.2 MB - LeetHub

--- 15356-780-73-set-matrix-zeroes/15356-780-73-set-matrix-zeroes.js
+++ 15356-780-73-set-matrix-zeroes/15356-780-73-set-matrix-zeroes.js
@@ -3,34 +3,59 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-    // loop over the matrix cells (row by row with col by col)
+    // loop over the matrix cells (row by row with col by col) and if the cell is 0, set its first element in the row and col as 0 (use firstRow/Col variable if needed)
+    let firstRow = false;
+    let firstCol = false;
+
     for(let row = 0; row < matrix.length; row++) {
         for(let col = 0; col < matrix[0].length; col++) {
             const curr = matrix[row][col];
-            if(curr === 0) updateInPlace(row, col);
+
+            if (curr === 0) {
+                firstRow ||= row === 0;
+                firstCol ||= col === 0;
+                
+                if (row > 0 && col > 0) {
+                    matrix[row][0] = 0;
+                    matrix[0][col] = 0;
+                }
+            }
         }
     }
 
-    // spread out horizontally and vertically setting non 0s to -1s
-    function updateInPlace(i, j) {
-        // update horizontally
-        for(let col = 0; col < matrix[0].length; col++) {
-            if(col === j || matrix[i][col] === 0) continue;
-            matrix[i][col] = Infinity;
-        }
+    // set 0s to rows 
+    for(let row = 1; row < matrix.length; row++) {
+        const curr = matrix[row][0];
 
-        // update vertically
+        if(curr === 0) {
+            for(let col = 1; col < matrix[0].length; col++) {
+                matrix[row][col] = 0;
+            }
+        }
+    }
+
+    // set 0s to cols
+    for(let col = 1; col < matrix[0].length; col++) {
+        const curr = matrix[0][col];
+
+        if(curr === 0) {
+            for(let row = 1; row < matrix.length; row++) {
+                matrix[row][col] = 0;
+            }
+        }
+    }
+
+    // set 0s to first row
+    if(firstRow) {
+        for(let col = 0; col < matrix[0].length; col++) {
+            matrix[0][col] = 0;
+        }
+    }
+
+    // set 0s to first col
+    if(firstCol) {
         for(let row = 0; row < matrix.length; row++) {
-            if(row === i || matrix[row][j] === 0) continue;
-            matrix[row][j] = Infinity;
-        }
-    }
-
-    // convert all -1s to 0s
-    for(let row = 0; row < matrix.length; row++) {
-        for(let col = 0; col < matrix[0].length; col++) {
-            const curr = matrix[row][col];
-            if(curr === Infinity) matrix[row][col] = 0;
+            matrix[row][0] = 0;
         }
     }
 
