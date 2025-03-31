3803338d9096917ffe8173533ee6af8d0b7c5c4a
Jaehong Lee <contact@jaehonglee.com>
2025-03-31 00:34:40 -0700

    Time: 0 ms (100%), Space: 41.6 MB (10.99%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -2,32 +2,20 @@ class Solution {
     public int rob(int[] nums) {
         if(nums.length == 1) return nums[0];
 
+        return Math.max(robHelper(0, nums.length - 1, nums), robHelper(1, nums.length, nums));
+    }
+
+    private int robHelper(int start, int end, int[] nums) {
         int prev = 0;
         int beforePrev = 0;
-        int ans = 0;
 
-        // loop over the array but without the last house
-        for(int idx = 0; idx < nums.length - 1; idx++) {
-            // set current index max nums as max of (nums[idx] + nums[idx - 2], nums[idx - 1])
+        for(int idx = start; idx < end; idx++) {
             int curr = Math.max(nums[idx] + beforePrev, prev);
             
             beforePrev = prev;
             prev = curr;
         }
-        ans = prev;
-        prev = 0;
-        beforePrev = 0;
-        
-        // repeat the above step but without the first house
-        for(int idx = 1; idx < nums.length; idx++) {
-            int curr = Math.max(nums[idx] + beforePrev, prev);
 
-            beforePrev = prev;
-            prev = curr;
-        }
-
-        // return max of the two iteration results
-        ans = Math.max(ans, prev);
-        return ans;
+        return prev;
     }
 }
