81e24bdba130a004da6f98a5460ece480c50d11d
Jaehong Lee <contact@jaehonglee.com>
2025-04-04 15:22:45 -0700

    Time: 4 ms (89.67%), Space: 58.6 MB (22.6%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,16 +1,17 @@
 class Solution {
     public int findDuplicate(int[] nums) {
         // define slow and fast pointers
-        int slow = nums[0];
-        int fast = nums[nums[0]];
+        int slow = 0;
+        int fast = 0;
 
-        // starting at index 0, move slow and fast pointers until cycle detection
-        while(slow != fast) {
+        // starting at index 0, move slow and fast pointers until they meet
+        do {
             slow = nums[slow];
             fast = nums[nums[fast]];
-        }
+        } while(slow != fast);
 
-        fast = 0;
+        // based on Floyd's algorithm, distance between the start to entry of cycle and distance between the entry of cycle to where the pointers met is the same
+        slow = 0;
         while(slow != fast) {
             slow = nums[slow];
             fast = nums[fast];
