c6a3a10b34ba0b2e30f126013a814bdc400343a5
Jaehong Lee <contact@jaehonglee.com>
2025-04-18 23:49:08 -0700

    Time: 0 ms (100%), Space: 40.8 MB (8.84%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -3,7 +3,6 @@ class Solution {
     // Find the xor of a and b
     // Add the found xor value with carry bits 
     // xor value will be stored to a and b will storing carry bits
-
     public int getSum(int a, int b) {
         while (b != 0) {
             int carry = (a & b) << 1;
