eff8c2075a3961328fb679d17fb3145c8e28d8c1
Jaehong Lee <contact@jaehonglee.com>
2025-05-09 23:17:29 -0700

    Time: 15 ms (44.63%), Space: 44.6 MB (83.66%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -5,22 +5,31 @@ class Solution {
         //      until the length of curr substring - most frequent > k
         char[] charArr = s.toCharArray();
         int[] count = new int[26];
-        int maxCount = 0, maxLength = 0;
         int left = 0, right = 0;
+        int ans = 0;
 
         while(right < charArr.length) {
             count[charArr[right] - 'A']++;
-            maxCount = Math.max(maxCount, count[charArr[right] - 'A']);
 
-            while((right - left + 1) - maxCount > k) {
-                count[s.charAt(left) - 'A']--;
+            while((right - left + 1) - findMax(count) > k) {
+                count[charArr[left] - 'A']--;
                 left++;
             }
 
-            maxLength = Math.max(maxLength, right - left + 1);
+            ans = Math.max(ans, right - left + 1);
             right++;
         }
 
-        return maxLength;
+        return ans;
+    }
+
+    private int findMax(int[] arr) {
+        int max = 0;
+
+        for(int num : arr) {
+            max = Math.max(max, num);
+        }
+
+        return max;
     }
 }
