9a60db27e7bed32f8a08251d58b3f31e85def07d
Jaehong Lee <contact@jaehonglee.com>
2025-09-12 16:22:18 -0700

    Time: 7 ms (41.99%), Space: 44.4 MB (71.08%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
@@ -1,35 +1,41 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        // Bellman Ford's Approach:
-
-        // Set prices & temp prices arrays
-        // Initial state: starting node is 0 with others Infinity
-        int[] prices = new int[n];
-        int max = Integer.MAX_VALUE;
+//         Define adjacency list
+        Map<Integer, List<int[]>> adjacencyList = new HashMap<>();
         
-        Arrays.fill(prices, max);
-        prices[src] = 0;
-
-        // Loop for k + 1 times
-        for(int i = 0; i < k + 1; i++) {
-            int[] tempPrices = Arrays.copyOf(prices, n);
-
-            // Go over every edge and update temp if the starting node is not Infinity
-            for(int[] flight : flights) {
-                int from = flight[0];
-                int to = flight[1];
-                int price = flight[2];
-
-                if(prices[from] != max) {
-                    tempPrices[to] = Math.min(tempPrices[to], prices[from] + price);
-                }
-            }
-
-            // Update prices array to be temp prices 
-            prices = tempPrices;
+        for(int[] flight: flights) {
+            int from = flight[0];
+            int to = flight[1];
+            int price = flight[2];
+            
+            if (!adjacencyList.containsKey(from)) adjacencyList.put(from, new ArrayList<>());
+            adjacencyList.get(from).add(new int[]{to, price});
         }
+        
+//         Dijkstra's Algorithm - Set all node estimates to Infinity except src & Start from the src (use minheap)
+        int[] stops = new int[n];
+        Arrays.fill(stops, Integer.MAX_VALUE);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        pq.offer(new int[] { 0, src, 0 });
 
-        // Return the destination node 
-        return prices[dst] == max ? -1 : prices[dst];
+        while(!pq.isEmpty()) {
+            int[] temp = pq.poll();
+            int dist = temp[0];
+            int node = temp[1];
+            int steps = temp[2];
+
+            if (steps >= stops[node] || steps > k + 1) continue;
+
+            stops[node] = steps;
+
+            if (node == dst) return dist;
+
+            if (!adjacencyList.containsKey(node)) continue;
+
+            for (int[] a : adjacencyList.get(node)) {
+                pq.offer(new int[] { dist + a[1], a[0], steps + 1 });
+            }
+        }
+        return -1;
     }
 }
