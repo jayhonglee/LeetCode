7c78ea9ab3641e89f9ac159c62ec7f75c487e0b3
Jaehong Lee <contact@jaehonglee.com>
2025-09-14 19:55:26 -0700

    Time: 91 ms (15.12%), Space: 64.2 MB (12.01%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.ts
@@ -0,0 +1,24 @@
+function permuteUnique(nums: number[]): number[][] {
+    // Create backtrack function
+    const backtrack = function(nums: number[], curr: number[]): void {
+        if(!nums.length && !seen.has(curr.join(","))) {
+            seen.add(curr.join(","));
+            ans.push(curr);
+            return;
+        }
+
+        for(const [idx, num] of nums.entries()) {
+            curr.push(num);
+            backtrack([...nums.filter((_, i) => i !== idx)], [...curr]);
+            curr.pop();
+        }
+    }
+
+    // Call backtrack function
+    let ans = [];
+    const seen = new Set();
+    backtrack([...nums], []);
+
+    // Return ans array
+    return ans;
+};
