5f395638c32c73868144e05750811ef81172425a
Jaehong Lee <contact@jaehonglee.com>
2025-10-24 12:14:44 -0700

    Time: 64 ms (29.83%), Space: 58.7 MB (55.7%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -3,23 +3,23 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    const hashmap = new Map();
-    let read = 0;
-    for(let write = 0; write < nums.length; write++) {
-        while(hashmap.get(nums[read]) >= 2) {
-            read++;
-        }
-        
+    // Two Pointers Solution:
+    // Time = O(n) & Space = O(1)
+    // Read pointer just iterates over the nums 
+    // Write pointer points to the index of the next valid element to come
+    // If an invalid element is found, the write pointer doesn't iterate and still point to the index where it should get updated with the correct element and read pointer continues to iterate.
+    // Once the right element is found, the new element is written to the write pointer.
 
-        if(read >= nums.length) {
-            nums.splice(write);
-            break;
-        } 
-        nums[write] = nums[read];
-        
-        if(!hashmap.get(nums[read])) hashmap.set(nums[read], 0);
-        hashmap.set(nums[read], hashmap.get(nums[read]) + 1);
-        
-        read++;
+    if(nums.length <= 2) return nums.length;
+    
+    let write = 2;
+
+    for(let read = 2; read < nums.length; read++) {
+        if(nums[read] !== nums[write - 2]) {
+            nums[write] = nums[read];
+            write++;
+        }
     }
+
+    return write;
 };
