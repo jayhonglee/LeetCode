049c1b0667cf9aec7a996fc69f03697a78325c16
Jaehong Lee <contact@jaehonglee.com>
2025-10-27 21:04:17 -0700

    Time: 24 ms (5.08%), Space: 68.6 MB (5.08%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -4,20 +4,29 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-    function applySameStyle(str) {
-        let hashmap = new Map(), finalStr = "", i = 0;
-        
+    const convertString = (str) => {
+        const hashmap = new Map();
+        let counter = 0;
+        let string = "";
+
         for(const char of str) {
-            if(!hashmap.has(char)) {
-                hashmap.set(char, i);        
-                i++;
-            }
+            if(hashmap.has(char)) string += hashmap.get(char);
             
-            finalStr += (` ${hashmap.get(char)}`);
+            hashmap.set(char, counter);
+            counter++;
+
+            string += hashmap.get(char);
         }
-        
-        return finalStr;
+
+        return string;
     }
-    
-    return applySameStyle(s) === applySameStyle(t);
+
+    // Convert the characters to numbers by mapping them by incrementing order
+    let sStr = convertString(s);
+
+    // Convert the characters to numbers by mapping them by incrementing order
+    let tStr = convertString(t);
+
+    // Return true if the two numbered strings match or not
+    return sStr === tStr;
 };
