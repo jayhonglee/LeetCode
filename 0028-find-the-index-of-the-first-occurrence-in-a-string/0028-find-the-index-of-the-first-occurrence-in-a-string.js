07850178cf302d644275ac94c929ab168349025b
Jaehong Lee <contact@jaehonglee.com>
2025-10-27 20:51:33 -0700

    Time: 1 ms (18.64%), Space: 54.6 MB (22.92%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,18 +4,19 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
+    // Iterate over each charater in haystack
     for(let i = 0; i < haystack.length; i++) {
-        let j = 0;
         let k = i;
-        while(haystack[k] == needle[j]) {
-            if(j >= needle.length - 1) {
-                return i
-            }
-            
-            k++;
+        let j = 0;
+        
+        // While the current character is equal to the corresponding character of the needle, continue, if not break
+        while(haystack[k] === needle[j]) {
+            if(j === needle.length - 1) return i;
+
             j++;
+            k++;
         }
     }
-    
+
     return -1;
 };
