2c1bdb3d9f1373153a0bf6fe84d4a70579ec508d
Jaehong Lee <contact@jaehonglee.com>
2025-10-27 20:26:39 -0700

    Time: 1361 ms (5.28%), Space: 80.5 MB (7.45%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.js
@@ -0,0 +1,42 @@
+/**
+ * @param {character[][]} matrix
+ * @return {number}
+ */
+var maximalSquare = function(matrix) {
+    // Optimal Solution
+
+    const hashmap = new Map();
+    const rowColStringify = (row, col) => row + "," + col;
+    const isInBound = (row, col) => row >= 0 && col >= 0 && row < matrix.length && col < matrix[0].length;
+
+    // LargestArea Function:
+    // - Find the right, bottom, and bottom right cell's minimum value and if the current cell is 1, find the square root of the value and + 1 and square to the current cell. 
+    // - Store the value to cache
+    const largestArea = (row, col) => {
+        if(hashmap.has(rowColStringify(row, col))) return hashmap.get(rowColStringify(row, col));
+        if(!isInBound(row, col) || matrix[row][col] === '0') return 0;
+
+        const right = largestArea(row, col + 1);
+        const bottomRight = largestArea(row + 1, col + 1);
+        const bottom = largestArea(row + 1, col);
+
+        const min = Math.min(right, bottomRight, bottom);
+        console.log(row, " ", col, min);
+
+        const area = Math.pow(Math.sqrt(min) + 1, 2);
+
+        hashmap.set(rowColStringify(row, col), area);
+        return area;
+    }
+
+    // Iterate over each cell.
+    // For each cell, find the largest sqaure of 1's as the cell being the top left cell.
+    let ans = 0;
+    for(let row = 0; row < matrix.length; row++) {
+        for(let col = 0; col < matrix[0].length; col++) {
+            ans = Math.max(ans, largestArea(row, col));
+        }
+    }
+
+    return ans;
+};
