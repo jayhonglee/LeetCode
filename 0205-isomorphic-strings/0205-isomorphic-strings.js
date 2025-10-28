f23275b3df8970fd0c8dccfca3c6ff937aa07104
Jaehong Lee <contact@jaehonglee.com>
2025-10-27 21:22:34 -0700

    Time: 7 ms (48.28%), Space: 56.5 MB (33.82%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -4,29 +4,48 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-    const convertString = (str) => {
-        const hashmap = new Map();
-        let counter = 0;
-        let string = "";
+    // Not Optimal By a Constant Factor
+    // const convertString = (str) => {
+    //     const hashmap = new Map();
+    //     let counter = 0;
+    //     let string = "";
 
-        for(const char of str) {
-            if(hashmap.has(char)) string += hashmap.get(char);
+    //     for(const char of str) {
+    //         if(hashmap.has(char)) string += hashmap.get(char);
             
-            hashmap.set(char, counter);
-            counter++;
+    //         hashmap.set(char, counter);
+    //         counter++;
 
-            string += hashmap.get(char);
-        }
+    //         string += hashmap.get(char);
+    //     }
 
-        return string;
+    //     return string;
+    // }
+
+    // // Convert the characters to numbers by mapping them by incrementing order
+    // let sStr = convertString(s);
+
+    // // Convert the characters to numbers by mapping them by incrementing order
+    // let tStr = convertString(t);
+
+    // // Return true if the two numbered strings match or not
+    // return sStr === tStr;
+
+    // Optimal Solution
+    const sCode = new Array(256);
+    const tCode = new Array(256);
+    let sCounter = 0;
+    let tCounter = 0;
+
+    for(let i = 0; i < s.length; i++) {
+        const sCharCode = s.charAt(i);
+        const tCharCode = t.charAt(i);
+
+        if(!sCode[sCharCode]) sCode[sCharCode] = ++sCounter;
+        if(!tCode[tCharCode]) tCode[tCharCode] = ++tCounter;
+
+        if (sCode[sCharCode] !== tCode[tCharCode]) return false;
     }
 
-    // Convert the characters to numbers by mapping them by incrementing order
-    let sStr = convertString(s);
-
-    // Convert the characters to numbers by mapping them by incrementing order
-    let tStr = convertString(t);
-
-    // Return true if the two numbered strings match or not
-    return sStr === tStr;
+    return true;
 };
