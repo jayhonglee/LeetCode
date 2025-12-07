35e5f36ad95575a817209002236f4ce09bb21e57
Jaehong Lee <contact@jaehonglee.com>
2025-12-07 00:11:21 -0800

    Time: 7 ms (56.45%), Space: 63.8 MB (56.79%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.js
+++ 0997-find-the-town-judge/0997-find-the-town-judge.js
@@ -4,24 +4,38 @@
  * @return {number}
  */
 var findJudge = function(n, trust) {
-    // Loop over the trust array & build adjacency list & trusted counter for each person
-    const trustList = new Array(n);
-    const trustedCounter = new Array(n).fill(0);
+    // Accepted solution (can be improved)
+    // // Loop over the trust array & build adjacency list & trusted counter for each person
+    // const trustList = new Array(n);
+    // const trustedCounter = new Array(n).fill(0);
 
-    for(let i = 0; i < n; i++) {
-        trustList[i] = [];
-    }
+    // for(let i = 0; i < n; i++) {
+    //     trustList[i] = [];
+    // }
 
-    for(const [a, b] of trust) {
-        trustList[a - 1].push(b);
-        trustedCounter[b - 1]++;
-    }
+    // for(const [a, b] of trust) {
+    //     trustList[a - 1].push(b);
+    //     trustedCounter[b - 1]++;
+    // }
 
-    // Loop over the adjList again and check if there is a person who doesnt trust anyone and is trusted by all other people
-    for(let i = 0; i < n; i++) {
-        if(trustList[i].length === 0 && trustedCounter[i] === n - 1) return i + 1;
-    }
+    // // Loop over the adjList again and check if there is a person who doesnt trust anyone and is trusted by all other people
+    // for(let i = 0; i < n; i++) {
+    //     if(trustList[i].length === 0 && trustedCounter[i] === n - 1) return i + 1;
+    // }
     
-    // Return -1 if no judge exists
+    // // Return -1 if no judge exists
+    // return -1;
+
+    // Optimal solution
+    const score = new Array(n).fill(0);
+
+    for(const [a,b] of trust) {
+        score[a - 1]--;
+        score[b - 1]++;
+    }
+
+    for (let i = 0; i < n; i++) {
+        if (score[i] === n - 1) return i + 1;
+    }
     return -1;
 };
