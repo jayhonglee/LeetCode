3f845fde8d23a589a6c96f66ce725c0da72726b6
Jaehong Lee <contact@jaehonglee.com>
2026-01-17 02:00:16 -0800

    Time: 18 ms (37.84%), Space: 81.4 MB (10.81%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.js
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.js
@@ -3,30 +3,64 @@
  * @return {number[]}
  */
 var canSeePersonsCount = function(heights) {
-//     Define variables
-    let stack = [], result = [];
-    
-//     Loop from right to left over heights
-//     Before continuing add current to the stack
-//          While current > stack.peak pop()
-//     Return stack.peak >= current ? 1 : stack.length to result array 
+    // Brute force: 
+    // For each person, iterate over each peron on the right 
+    // and store the visible count using local maximum 
+    // if the new local maximum is taller than current person, cannot see any further
+
+    // Code (Correct logic but TLE):
+    // const ans = [];
+
+    // for(let i = 0; i < heights.length; i++) {
+    //     let visibleCount = 0;
+    //     let localMax = -1;
+    //     for(let j = i + 1; j < heights.length; j++) {
+    //         if(heights[j] > localMax) {
+    //             localMax = heights[j];
+    //             visibleCount++;
+    //         }
+
+    //         if(localMax > heights[i]) break;
+    //     }
+
+    //     ans.push(visibleCount);
+    // }
+
+    // return ans;
+
+    // Optimal Solution:
+    // Iterating from right to left, we'll use a strictly decreasing monotonic stack
+    // For each person
+    // 1) If the stack is empty = noone visible
+    // 2) If the stack isn't empty
+    //    - If the current person isn't taller than stack.top() add to the stack and visible = 1
+    //    - If the current peron is taller than stack.top() (while it's true) and count++
+    //      - After the while loop is over, if the stack isn't empty count++
+
+    const stack = [];
+    const ans = [];
     for(let i = heights.length - 1; i >= 0; i--) {
-        const current = heights[i];
-        
-        let j = stack.length - 1;
-        let count = 0;
-        let found = false;
-        while(!found && stack.length && stack[j]) {
-            if(stack[j] >= current) found = true;
-            count++;
-            j--;
+        const curr = heights[i];
+        let visibleCount = 0;
+
+        if(!stack.length) stack.push(curr);
+        else {                
+            if(curr < stack[stack.length - 1]) {
+                stack.push(curr);
+                visibleCount = 1;
+            } else {
+                while(stack.length && curr > stack[stack.length - 1]) {
+                    stack.pop();
+                    visibleCount++;
+                }
+
+                if(stack.length) visibleCount++;
+                stack.push(curr);
+            }
         }
-        
-        
-        result[i] = stack[stack.length - 1] && stack[stack.length - 1] >= current ? 1 : count;
-        while(stack.length && current >= stack[stack.length - 1]) stack.pop();
-        stack.push(current);
+
+        ans.push(visibleCount);
     }
-    
-    return result;
+
+    return ans.reverse();
 };
