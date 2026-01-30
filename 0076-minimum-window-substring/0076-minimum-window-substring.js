e53e709ebaba322c842b9ae14650104574f68472
Jaehong Lee <contact@jaehonglee.com>
2026-01-31 06:47:41 +0900

    Time: 25 ms (58.04%), Space: 60.4 MB (21.35%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.js
+++ 0076-minimum-window-substring/0076-minimum-window-substring.js
@@ -4,42 +4,53 @@
  * @return {string}
  */
 var minWindow = function(s, t) {
-    const m = s.length;
-    const n = t.length;
-    
-    if(m < n) return "";
-    
-    let dict = {};
-    for(let i = 0; i < n; i++) {
-        const char = t[i];
-        dict[char] = (dict[char] || 0) + 1;
-    }
-    
-    let ans = "";
-    let right = 0;
-    let count = 0;
-    let temp = "";
-    for(let left = 0; left <= m - n; left++) {
-        while(right < m && count < n) {
-            const currChar = s[right];
-            temp += currChar;
-            if(dict[currChar] !== undefined) {
-                dict[currChar]--;
-                if(dict[currChar] >= 0) count++;
+    // Intuition)
+    // - Sliding window (right pointer expands window, left pointer shrinks window)
+    // - (Condition) Window expands if window not valid
+    // - (Condition) Window shrinks while window valid
+    // - Validation using need counter (hashmap) & needMap
+    // - Only update needMap if the need counter = 0
+    // - Ans [number, number] to keep shortest substring indexes
+
+    // Define need counter & needMap & ans
+    let need = t.length;
+    let needMap = new Map();
+    let ans = [-1, -1];
+
+    for(const char of t) needMap.set(char, (needMap.get(char) || 0) + 1);
+
+    let left = 0;
+
+    // Right pointer loop over the entire s
+    for(let right = 0; right < s.length; right++) {
+        const rightChar = s[right];
+
+        // Need counter update logic & needMap
+        if(needMap.has(rightChar)) {
+            needMap.set(rightChar, needMap.get(rightChar) - 1);
+            if(needMap.get(rightChar) >= 0) need--;
+        }
+
+        
+
+        // While window valid, left pointer shrinks window
+        while(need === 0) {
+            // Update ans
+            if(ans[0] === -1 || (right - left + 1) < (ans[1] - ans[0] + 1)) {
+                ans = [left, right];
             }
-            right ++;
+
+            // Update need counter & needMap
+            const leftChar = s[left];
+            if(needMap.has(leftChar)) {
+                needMap.set(leftChar, needMap.get(leftChar) + 1);
+                if(needMap.get(leftChar) > 0) need++;
+            }
+
+            left++;
         }
-        
-        if(count === n && (ans.length === 0 || temp.length < ans.length)) {
-            ans = temp;
-        }
-        
-        if(dict[s[left]] !== undefined) {
-            dict[s[left]]++;
-            if(dict[s[left]] > 0) count--;
-        }
-        temp = temp.slice(1);
     }
-    
-    return ans;
+
+    // Return ans substring (If ans is invalid, return empty string)
+    return ans[0] === -1 ? "" : s.slice(ans[0], ans[1] + 1);
 };
