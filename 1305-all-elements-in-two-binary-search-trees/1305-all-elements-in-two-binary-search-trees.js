ad35f70fae88cc138ca241e07e8d51435f56e0c4
Jaehong Lee <contact@jaehonglee.com>
2026-08-24 23:23:54 -0700

    Time: 8 ms (81.48%), Space: 68.8 MB (14.81%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.js
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.js
@@ -12,33 +12,69 @@
  * @return {number[]}
  */
 var getAllElements = function(root1, root2) {
-    // BFS (Function)
-    // Returns a list of all nodes
-    const bfs = (root) => {
-        let queue = [root];
-        const list = [];
+    // Solution 1 (Not optimal)
+    // // BFS (Function)
+    // // Returns a list of all nodes
+    // const bfs = (root) => {
+    //     let queue = [root];
+    //     const list = [];
 
-        while(queue.length) {
-            const nextQueue = [];
+    //     while(queue.length) {
+    //         const nextQueue = [];
 
-            for(const node of queue) {
-                list.push(node.val);
+    //         for(const node of queue) {
+    //             list.push(node.val);
 
-                if(node.left) nextQueue.push(node.left);
-                if(node.right) nextQueue.push(node.right);
-            }
+    //             if(node.left) nextQueue.push(node.left);
+    //             if(node.right) nextQueue.push(node.right);
+    //         }
 
-            queue = nextQueue;
-        }
+    //         queue = nextQueue;
+    //     }
 
-        return list;
+    //     return list;
+    // }
+
+    // // BFS function ran on both roots
+    // const root1List = root1 ? bfs(root1) : [];
+    // const root2List = root2 ? bfs(root2) : [];
+
+    // // Merge two lists and sort them in ascending order
+    // const ans = root1List.concat(root2List).sort((a,b) => a - b);
+    // return ans;
+
+    // Solution 2
+    // DFS (Function) - returns ascending array
+    const dfs = (root, array) => {
+        if(!root) return array;
+
+        if(root.left) dfs(root.left, array);
+        array.push(root.val);
+        if(root.right) dfs(root.right, array);
+
+        return array;
     }
 
-    // BFS function ran on both roots
-    const root1List = root1 ? bfs(root1) : [];
-    const root2List = root2 ? bfs(root2) : [];
+    // Run DFS on both of the roots 
+    const array1 = dfs(root1, []);
+    const array2 = dfs(root2, []);
+    
+    // Compare both arrays and merge them in ascending in O(N) Complexity
+    const ans = [];
+    let ptr1 = 0;
+    let ptr2 = 0;
+    while(ptr1 < array1.length || ptr2 < array2.length) {
+        const value1 = array1[ptr1];
+        const value2 = array2[ptr2];
 
-    // Merge two lists and sort them in ascending order
-    const ans = root1List.concat(root2List).sort((a,b) => a - b);
+        if(ptr2 >= array2.length || value1 < value2) {
+            ans.push(value1);
+            ptr1++;
+        } else {
+            ans.push(value2);
+            ptr2++;
+        }
+    }
+
     return ans;
 };
